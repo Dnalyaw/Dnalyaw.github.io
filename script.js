def460b9242a74dca2dc716416ec0509b5db3067
Dnalyaw <waylandla@gmail.com>
2023-09-27 21:00:03 -0700

    added some javascript

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ toggleButton.addEventListener('click', () => {
   introduction.classList.toggle('active')
   introText.classList.toggle('active')
 })
+
+var i = 0;
+var txt = 'Welcome to my website, my name is'; /* The text */
+var bigTxt = 'Wayland.'
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typedTxt").textContent += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
+
+setInterval(function () {
+  var birthday = new Date("11/17/2005");
+  var today = new Date();
+  var yearsAlive = Math.round((today - birthday)*1000000000/31556952000)/1000000000;
+  document.getElementById("age").textContent = yearsAlive;
+}, 100);
