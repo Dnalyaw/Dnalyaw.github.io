532612459a58524dc060676f66dec4fcd2b2c83e
Dnalyaw <waylandla@gmail.com>
2023-10-02 13:46:19 -0700

    party added

--- script.js
+++ script.js
@@ -29,3 +29,39 @@ setInterval(function () {
   var yearsAlive = Math.round((today - birthday)*1000000000/31556952000)/1000000000;
   document.getElementById("age").textContent = yearsAlive;
 }, 100);
+
+
+const seeMore = document.getElementById("seeMore");
+var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+var j = 0;
+
+var isParty = false;
+function party() {
+
+  if(!isParty) {
+    isParty = true;
+    seeMore.textContent = "Click to stop the party!";  
+    var id = setInterval(showColors, 1000); 
+    
+  } else {
+    if(isParty) {
+      isParty = false;
+      seeMore.textContent = "Click to start the party!";
+      document.querySelectorAll("h1")[0].style.color = "black";
+      
+    }
+  }
+}
+
+function showColors() {
+  if (isParty) {
+    document.querySelectorAll("h1")[0].style.color = colors[j];
+  j++;
+  if (j > 5) {
+    j = 0;
+  }
+  }
+  
+}
+
+
